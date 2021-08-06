91f724fbd0cabf0d5e9c31d5cd625430d6f14837
Ross Wardrup <minorsecond@gmail.com>
2021-08-06 16:32:36 -0600

    Less than operator overload for Datetime objects

--- src/Datetime.cpp
+++ src/Datetime.cpp
@@ -15,4 +15,29 @@ std::string Datetime::get_timestamp() const {
     return std::to_string(year) + zp_month + zp_day + zp_hour + zp_minute + zp_sec;
 }
 
+bool Datetime::operator<(Datetime &dtobj) const {
+    /*
+     * Compare dates and determine if dtobj is less than lhs date.
+     * @param &dtobj: a reference to a datetime object.
+     * @return: True of dtobj is sooner than this date.
+     */
+
+    if (year < dtobj.year) {
+        return true;
+    } else if (year == dtobj.year) {
+        if (month < dtobj.month) {
+            return true;
+        } else if (month == dtobj.month && day < dtobj.day) {
+            return true;
+        } else if (month == dtobj.month && day == dtobj.day && hour < dtobj.hour) {
+            return true;
+        } else if (month == dtobj.month && day == dtobj.day && hour == dtobj.hour && minute < dtobj.minute) {
+            return true;
+        } else if (month == dtobj.month && day == dtobj.day && hour == dtobj.hour && minute == dtobj.minute && second < dtobj.minute) {
+            return true;
+        }
+    }
+    return false;
+}
+
 Datetime::Datetime() = default;
--- src/Datetime.h
+++ src/Datetime.h
@@ -22,6 +22,7 @@ public:
     Datetime(int year, int month, int day, int hour, int min, int sec)
         : year{year}, month{month}, day{day}, hour{hour}, minute{min}, second{sec} {};
     std::string get_timestamp() const ;
+    bool operator<(Datetime &dtobj) const;
 };
 
 
